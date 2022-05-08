557d1d17281f06efa935230f813cdc36bda385da
Abinash <abinashbhullar.as@gmail.com>
2022-05-08 13:30:08 -0600

    Made Enumerated Types in Event Class consistant with coding style, and included ostream into event to make it compatible with future versions of the VS compiler

--- AEngine/src/AEngine/EventHandling/Event.h
+++ AEngine/src/AEngine/EventHandling/Event.h
@@ -10,6 +10,7 @@
 #include "../Core.h"
 
 #include <string>
+#include <ostream>
 #include <functional>
 
 /**
@@ -22,21 +23,21 @@ namespace AEngine {
 	 * All the differnt types of blocking events.
 	 */
 	enum class EventType {
-		None = 0,
-		WindowClose,
-		WindowResize,
-		WindowFocus,
-		WindowLostFocus,
-		WindowMoved,
-		AppTick,
-		AppUpdate,
-		AppRender,
-		KeyPressed,
-		KeyReleased,
-		MouseButtonPressed,
-		MouseButtonReleased,
-		MouseMoved,
-		MouseScrolled
+		NONE = 0,
+		WINDOW_CLOSE,
+		WINDOW_RESIZE,
+		WINDOW_FOCUS,
+		WINDOW_LOST_FOCUS,
+		WINDOW_MOVED,
+		APP_TICK,
+		APP_UPDATE,
+		APP_RENDER,
+		KEY_PRESSED,
+		KEY_RELEASED,
+		MOUSE_BUTTON_PRESSED,
+		MOUSE_BUTTON_RELEASED,
+		MOUSE_MOVED,
+		MOUSE_SCROLLED
 	};
 
 	/**
@@ -44,12 +45,12 @@ namespace AEngine {
 	 * Bit fields for event catagories. The BIT Macro is just shifting 1 by X bits.
 	 */
 	enum EventCategory {
-		None = 0,
-		EventCategoryApplication = BIT(0),
-		EventCategoryInput = BIT(1),
-		EventCategoryKeyboard = BIT(2),
-		EventCategoryMouse = BIT(3),
-		EventCategoryMouseButton = BIT(4)
+		NONE = 0,
+		EVENT_CATEGORY_APPLICATION = BIT(0),
+		EVENT_CATEGORY_INPUT = BIT(1),
+		EVENT_CATEGORY_KEYBOARD = BIT(2),
+		EVENT_CATEGORY_MOUSE = BIT(3),
+		EVENT_CATEGORY_MOUSE_BUTTON = BIT(4)
 	};
 
 	#define EVENT_CLASS_TYPE(type) static EventType GetStaticType() { return EventType::##type; }\
