e678e0286bc3b843e92d16c34d44daf5683e7555
Abinash <abinashbhullar.as@gmail.com>
2022-08-31 22:27:01 -0600

    Started work on Scene And Camera

--- AEngine/src/AEngine/components/Camera.cpp
+++ AEngine/src/AEngine/components/Camera.cpp
@@ -1,8 +1,39 @@
 #include "aepch.h"
 #include "Camera.h"
 
+#include <glm/ext/matrix_clip_space.hpp>
+#include <glm/ext/matrix_transform.hpp>
+
 namespace AEngine {
-    void Camera::update(float _delta) {}
+
+    Camera::Camera(float _fov, float _aspect_ratio, float _z_near, float _z_far) {
+        projection     = glm::perspective(_fov, _aspect_ratio, _z_near, _z_far);
+        view           = glm::mat4(1.0f);
+        viewProjection = projection * view;
+    }
+
+    Camera::Camera(float _left, float _right, float _bottom, float _top, float _z_near, float _z_far) {
+        projection     = glm::ortho(_left, _right, _bottom, _top, _z_near, _z_far);
+        view           = glm::mat4(1.0f);
+        viewProjection = projection * view;
+    }
+
+    Camera::~Camera() {}
+
+    void Camera::recalculateViewMatrix() {
+
+        glm::mat4 transform = translate(glm::mat4(1.0f), position) *
+                              rotate(glm::mat4(1.0f), glm::radians(rotation), glm::vec3(0, 0, 1));
+
+        view           = inverse(transform);
+        viewProjection = projection * view;
+
+    }
+
+    void Camera::update(float _delta) { }
+
     void Camera::render() {}
+
     void Camera::input(float _delta) {}
+
 }
--- AEngine/src/AEngine/components/Camera.h
+++ AEngine/src/AEngine/components/Camera.h
@@ -1,12 +1,28 @@
 #pragma once
+#include <glm/glm.hpp>
+
 #include "SceneComponent.h"
 
 namespace AEngine {
     class Camera : public SceneComponent {
-        virtual void update(float _delta) override;
-        virtual void render() override;
-        virtual void input(float _delta) override;
+        public:
+            Camera(float _fov, float _aspect_ratio, float _z_near, float _z_far);
+            Camera(float _left, float _right, float _bottom, float _top, float _z_near, float _z_far);
+
+            ~Camera();
+
+            void recalculateViewMatrix();
+
+            virtual void update(float _delta) override;
+            virtual void render() override;
+            virtual void input(float _delta) override;
         private:
-            
+            glm::mat4 projection;
+            glm::mat4 view;
+            glm::mat4 viewProjection;
+
+            glm::vec3 position = { 0.0f, 0.0f, 0.0f };
+
+            float rotation = 0.0f;
     };
 }
